4522748b8df105fde5c9ab2e8d669916e22af759
unknown <necmettin_kzldg@hotmail.com>
2026-05-20 17:38:25 +0200

    Some corrections and changes on Timer1 and PWM sample

--- Timer1 and PWM Example/Timer1 and PWM Example/main.c
+++ Timer1 and PWM Example/Timer1 and PWM Example/main.c
@@ -10,16 +10,18 @@
 
 #include "Source/328p_config.h"
 #include <avr/interrupt.h>
-#include <util/delay.h>
 
 #define OCRA_v						33999
+#define LED_COUNT 8
 
 static void gpio_init();
 static void pcint_init();
 static void timer1_init();
 
-uint8_t flag, led_no;
-uint16_t on_time, off_time;
+volatile uint16_t on_time = 0;
+volatile uint16_t off_time = OCRA_v;
+volatile uint8_t flag = 0;
+volatile uint8_t led_no = 0;
 
 int main(void)
 {
@@ -27,10 +29,6 @@ int main(void)
     pcint_init();
     timer1_init();
 	sei();
-	
-	on_time = 0;
-	flag = 0;
-	led_no = 0;
 	
     while (1) 
     {
@@ -45,6 +43,7 @@ static void gpio_init()
 	GPIO_D.port.value = port_ALL_LOW;
 	
 	GPIO_B.ddr.pins.pin5 = pin_INPUT;
+	GPIO_B.port.pins.pin5 = pin_HIGH; // Pull-up active
 }
 
 static void pcint_init()
@@ -55,24 +54,26 @@ static void pcint_init()
 
 static void timer1_init()
 {
-	TIMER1_MODE = timer1_mode_05_FAST_PWM_8_BIT;
+	TIMER1_MODE = timer1_mode_04_CTC_on_OCR1A;
 	TIMER1_clock = timer1_prescaling_8;
 	TIMER1_interrupt_OUTPUT_COMP_A = interrupt_ENABLE;
-	TIMER1_OCRA_value = OCRA_v;
+	//TIMER1_OCRA_value = OCRA_v;
+	TIMER1_OCRA_value = on_time;
+	//ICR1 = OCRA_v; // Top value
 }
 
-ISR(INT0_vect) {
+ISR(PCINT0_vect) {
 	
-    if (GPIO_B.pin.pins.pin5 == 1) 
+    if (GPIO_B.pin.pins.pin5 == 1) // 0 for pull-up
 	{
         GPIO_D.port.value = port_ALL_LOW;
         on_time = 0;
-        led_no = (led_no < 7) ? led_no + 1 : 0;
+        led_no = (led_no < LED_COUNT - 1) ? led_no + 1 : 0;
     }
 	
 }
 
-ISR(TIMER1_COMPA_vect){
+ISR(TIMER1_COMPA_vect){ //PB5 -  pin 13 on arduino
 	on_time = (on_time < OCRA_v) ? on_time + 50 : 0;
 	off_time = OCRA_v - on_time;
 	
